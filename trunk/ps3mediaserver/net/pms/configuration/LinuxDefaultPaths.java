87f541e69d2bc247be1f7b356497473f52e5cc6d
SubJunk <SubJunk@e04080f2-c846-11dd-89f9-7f0a91924d23>
2012-02-08 05:56:36 +0000

    Introduced PropertiesUtil to retrieve project properties.
    From Git: 1d1f941abdf3acf376222ee5c50aaeae74d32e88

--- trunk/ps3mediaserver/net/pms/configuration/LinuxDefaultPaths.java
+++ trunk/ps3mediaserver/net/pms/configuration/LinuxDefaultPaths.java
@@ -1,19 +1,12 @@
 package net.pms.configuration;
 
-import java.io.IOException;
-
-import net.pms.util.PmsProperties;
+import net.pms.util.PropertiesUtil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 class LinuxDefaultPaths implements ProgramPaths {
 	private static final Logger logger = LoggerFactory.getLogger(MacDefaultPaths.class);
-	
-	/**
-	 * General properties for the PMS project.
-	 */
-	private final PmsProperties projectProperties = new PmsProperties();
 
 	@Override
 	public String getEac3toPath() {
@@ -59,28 +52,6 @@ class LinuxDefaultPaths implements ProgramPaths {
 	public String getIMConvertPath() {
 		return "convert";
 	}
-
-	/**
-	 * Constructor makes sure the project properties are read
-	 */
-	public LinuxDefaultPaths() {
-		try {
-			// Read project properties resource file.
-			getProjectProperties().loadFromResourceFile("/resources/project.properties");
-		} catch (IOException e) {
-			logger.error("Could not load project.properties");
-		}
-	}
-	
-	/**
-	 * Returns the project properties object.
-	 *
-	 * @return The properties object.
-	 */
-	private PmsProperties getProjectProperties() {
-		return projectProperties;
-	}
-
 	/**
 	 * Returns the path where binaries can be found. This path differs between
 	 * the build phase and the test phase. The path will end with a slash unless
@@ -89,7 +60,7 @@ class LinuxDefaultPaths implements ProgramPaths {
 	 * @return The path for binaries.
 	 */
 	private String getBinariesPath() {
-		String path = getProjectProperties().get("project.binaries");
+		String path = PropertiesUtil.getProjectProperties().get("project.binaries");
 		
 		if (path != null && !"".equals(path)) {
 			if (path.endsWith("/")) {
