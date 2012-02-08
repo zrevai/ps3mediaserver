8812280a47d84ba7f4cd2df760d7a797559c2150
SubJunk <SubJunk@e04080f2-c846-11dd-89f9-7f0a91924d23>
2012-02-08 05:10:13 +0000

    Revert r1086
    I'm gonna go through and do each of the commits separately to try to figure out the bug where the program doesn't start
    There's probably a much easier way to do this on Git but I CBF learning it ATM so just gonna do it this long way

--- ps3mediaserver/net/pms/dlna/RootFolder.java
+++ ps3mediaserver/net/pms/dlna/RootFolder.java
@@ -60,7 +60,7 @@ import com.sun.jna.Platform;
 
 public class RootFolder extends DLNAResource {
 	private static final Logger logger = LoggerFactory.getLogger(RootFolder.class);
-	private final PmsConfiguration configuration = PMS.getConfiguration();
+	private PmsConfiguration configuration = PMS.getConfiguration();
 	private boolean running;
 
 	public RootFolder() {
@@ -732,19 +732,19 @@ public class RootFolder extends DLNAResource {
 				}
 			});
 
-			res.addChild(new VirtualVideoAction(Messages.getString("PMS.7"), configuration.getSkipLoopFilterEnabled()) {
-				@Override
-				public boolean enable() {
-					configuration.setSkipLoopFilterEnabled(!configuration.getSkipLoopFilterEnabled());
-					return configuration.getSkipLoopFilterEnabled();
-				}
-			});
-
 			res.addChild(new VirtualVideoAction(Messages.getString("TrTab2.28"), configuration.isDTSEmbedInPCM()) {
 				@Override
 				public boolean enable() {
 					configuration.setDTSEmbedInPCM(!configuration.isDTSEmbedInPCM());
 					return configuration.isDTSEmbedInPCM();
+				}
+			});
+
+			res.addChild(new VirtualVideoAction(Messages.getString("PMS.7"), configuration.getSkipLoopFilterEnabled()) {
+				@Override
+				public boolean enable() {
+					configuration.setSkipLoopFilterEnabled(!configuration.getSkipLoopFilterEnabled());
+					return configuration.getSkipLoopFilterEnabled();
 				}
 			});
 
@@ -762,7 +762,10 @@ public class RootFolder extends DLNAResource {
 			res.addChild(new VirtualVideoAction(Messages.getString("LooksFrame.12"), true) {
 				@Override
 				public boolean enable() {
-					PMS.get().reset();
+					try {
+						PMS.get().reset();
+					} catch (IOException e) {
+					}
 					return true;
 				}
 			});
